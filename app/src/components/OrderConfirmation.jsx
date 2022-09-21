3dd9d8feb9be547aada20177b79b680871081868
Annelie Yan <annelieyan@gmail.com>
2022-09-22 01:28:22 +0200

    NJ-XX Refactor orderconfirmation and display order data

--- app/src/components/OrderConfirmation.jsx
+++ app/src/components/OrderConfirmation.jsx
@@ -5,6 +5,7 @@ import useDidMountEffect from "../hooks/useDidMountEffect";
 function OrderConfirmation() {
   const { auth } = useContext(GlobalContext);
   const [data, setData] = useState({});
+
   const fetchData = async () => {
     let response = await fetch("/data/checkout");
     if (response.ok) {
@@ -40,16 +41,35 @@ function OrderConfirmation() {
     }
   };
 
-  useDidMountEffect(() => {
+  useEffect(() => {
     fetchData();
-  }, [auth.id]);
+  }, []);
 
   useDidMountEffect(postData, [data]);
+
+  console.log(data);
 
   return (
     <div className="container" id="order">
       <div className="order-content">
-        <h1>Order confirmation</h1>
+        <h1>
+          Thank you for ordering{auth.first_name && ", "} {auth.first_name}!
+        </h1>
+        <div>
+          {data.checkoutSession?.line_items.data.map((item, index) => {
+            return (
+              <div key={index}>
+                <p>
+                  {item.description} x {item.quantity}
+                </p>
+                <hr style={{ margin: "8px 0" }} />
+                <p style={{ fontSize: "18px" }}>
+                  Total: {item.amount_total / 100} {item.currency.toUpperCase()}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );

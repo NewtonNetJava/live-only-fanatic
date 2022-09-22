ee43463917816f37ff196ed000fee9a8106efead
Annelie Yan <annelieyan@gmail.com>
2022-09-22 20:20:27 +0200

    NJ-XX Add styling to back to top

--- app/src/components/BackToTopButton.jsx
+++ app/src/components/BackToTopButton.jsx
@@ -1,37 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import arrowUp from "../assets/arrow_upward.svg";
 
 function BackToTopButton() {
+  const [visible, setVisible] = useState(false);
 
-    const [visible, setVisible] = useState(false)
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
 
-    const toggleVisible = () => {
-        const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300) {
-            setVisible(true)
-        }
-        else if (scrolled <= 300) {
-            setVisible(false)
-        }
-    };
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-            /* you can also use 'auto' behaviour
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
                in place of 'smooth' */
-        });
-    };
-    window.addEventListener('scroll', toggleVisible);
+    });
+  };
 
-    return (
+  window.addEventListener("scroll", toggleVisible);
 
-        <div className="scrollButton" onClick={scrollToTop}>
-            <img src={arrowUp} id="arrowUp" alt="up button" />
-        </div>
+  if (!visible) return <></>;
 
-    );
+  return (
+    <button className="scrollButton" onClick={scrollToTop}>
+      <img src={arrowUp} id="arrowUp" alt="up button" />
+    </button>
+  );
 }
 
 export default BackToTopButton;
--- app/src/components/TicketsPage.jsx
+++ app/src/components/TicketsPage.jsx
@@ -1,59 +1,61 @@
 import useFetch from "../hooks/useFetch";
 import Ticket from "./Tickets";
-import { useEffect, useContext, useState } from 'react';
+import { useEffect, useContext, useState } from "react";
 import GlobalContext from "../context/GlobalContext";
-import BackToTopButton from "./BackToTopButton"
 
 function TicketsPage() {
-    const { error, isPending, data: tickets } = useFetch("/data/user_tickets_details");
-    const { auth } = useContext(GlobalContext);
-    const [filteredTickets, setFilteredTickets] = useState([]);
+  const {
+    error,
+    isPending,
+    data: tickets,
+  } = useFetch("/data/user_tickets_details");
+  const { auth } = useContext(GlobalContext);
+  const [filteredTickets, setFilteredTickets] = useState([]);
 
+  useEffect(() => {
+    async function load() {
+      let rawResponse = await fetch("/data/login");
+      if (rawResponse.status === 401) {
+        alert("Sign in to display your ticket(s)!");
+        window.location.replace("http://127.0.0.1:5173/");
+      }
+      if (rawResponse.ok) {
+        let response = await rawResponse.json();
+      }
+    }
+    load();
+  }, []);
 
-    useEffect(() => {
-        async function load() {
-            let rawResponse = await fetch('/data/login')
-            if (rawResponse.status === 401) {
-                alert("Sign in to display your ticket(s)!");
-                window.location.replace("http://127.0.0.1:5173/");
-            } if (rawResponse.ok) {
-                let response = await rawResponse.json();
-
-            }
+  useEffect(() => {
+    setTimeout(() => {
+      function filterTickets() {
+        if (tickets) {
+          const filTickets = tickets.filter(
+            (ticket) => ticket.user_id == auth.id
+          );
+          setFilteredTickets(filTickets);
         }
-        load()
-    }, [])
+      }
+      filterTickets();
+    }, 100);
+  }, [tickets]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            function filterTickets() {
-                if (tickets) {
-                    const filTickets = tickets.filter(ticket => ticket.user_id == auth.id)
-                    setFilteredTickets(filTickets)
-                }
-            }
-            filterTickets()
-        }, 100)
-    }, [tickets])
-
-    return (
-        <>
-            <h1 className="ticket-header">My tickets</h1>
-            <div className="container">
-                {auth.loggedIn && (
-                    <div className="card-container">
-                        {error && <div>{error}</div>}
-                        {isPending && <div>Loading...</div>}
-                        {filteredTickets &&
-                            filteredTickets.map((ticket) => <Ticket key={ticket} props={ticket} />)}
-                    </div>
-                )}
-                <BackToTopButton />
-
-            </div>
-        </>
-    );
-
-
+  return (
+    <>
+      <h1 className="ticket-header">My tickets</h1>
+      <div className="container">
+        {auth.loggedIn && (
+          <div className="card-container">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {filteredTickets &&
+              filteredTickets.map((ticket) => (
+                <Ticket key={ticket} props={ticket} />
+              ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 export default TicketsPage;

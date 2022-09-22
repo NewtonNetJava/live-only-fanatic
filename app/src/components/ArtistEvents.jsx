f93941bf75d065d746d0c562e811b05773550d80
Dannebanne94 <92047500+Dannebanne94@users.noreply.github.com>
2022-09-23 01:36:41 +0200

    NJ-XX: Rerender artists events in artists page

--- app/src/components/ArtistEvents.jsx
+++ app/src/components/ArtistEvents.jsx
@@ -8,18 +8,25 @@ export default function ArtistEvents(props) {
   const [events, setEvents] = useState([]);
   const id = parseInt(props["id"]);
 
+  console.log('events', events);
+  console.log('id', id);
+  
+
   const { data: concerts, error, isPending, getEvents } = useConcertData();
   useEffect(() => {
     getEvents();
   }, []);
 
+  console.log('concerts', concerts);
+
   useEffect(() => {
     if (!concerts) return;
     async function load() {
       setEvents(concerts.filter((event) => event.artist_id === id));
+      console.log('filtered concerts', concerts.filter((event) => event.artist_id === id) );
     }
     void load();
-  }, [concerts]);
+  }, [concerts, id]);
 
   return (
     <>
